6062f12c3bbfacadfde88ab82ec459aed1b61ec9
Jkilker <116875574+Jkilker@users.noreply.github.com>
2022-11-25 09:35:19 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,25 @@
+var x, y, r, speed;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    x = width/2;
+    y = height/2;
+    r = 200;
+    speed = 5;
 }
 
 function draw() {
     background(220);
 
+    speed = map(mouseY, 0, height, 1, 20);
+
+    y = y + speed;
+
+    if(y >= height + r/2){
+        y = - r/2
+    }
+
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(x, y, r);
 }
